1a6b04626ca72c28ae460574b17bdef23caa710e
AntixxTechHub <info@antixxtechhub.com>
2023-05-11 02:39:56 +0530

    cloud added

--- config/plugins.js
+++ config/plugins.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: "cloudinary",
       providerOptions: {
-        cloud_name: "do2....",
-        api_key: "64658.............",
-        api_secret: "TsLE...................",
+        cloud_name: "ds6j6gokv",
+        api_key: "969987261619884",
+        api_secret: "_For33eus3jpdwnqkOHg_PkbfDg",
       },
       actionOptions: {
         upload: {},

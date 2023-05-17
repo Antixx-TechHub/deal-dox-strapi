e6c0208acbd1e4001284e1d1e513701067173320
AntixxTechHub <info@antixxtechhub.com>
2023-05-18 01:08:19 +0530

    ckodenary added

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,19 @@
-// module.exports = ({ env }) => ({
-//   // ...
-//   upload: {
-//     // Update your cloudinary credentials here
-//     config: {
-//       provider: "cloudinary",
-//       providerOptions: {
-//       cloud_name: "antixx-techhub",
-//       api_key: "867656517325614",
-//       api_secret: "2wk2P3OGcGdsUo-Q_4Y4voIVE-U",
-//       },
-//       actionOptions: {
-//         upload: {},
-//         delete: {},
-//       },
-//     },
-//   },
-//   // ...
-// });
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    // Update your cloudinary credentials here
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+      cloud_name: "ds6j6gokv",
+      api_key: "969987261619884",
+      api_secret: "_For33eus3jpdwnqkOHg_PkbfDg",
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
